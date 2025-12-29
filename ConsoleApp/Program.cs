e374dcff3ef1483675bd16c673fbd5ef0ee3d8f5
Now <khj990322@gmail.com>
2025-12-30 04:27:40 +0900

    feat: LoopStatement.Run() 호출 추가

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -30,6 +30,7 @@ namespace ConsoleApp2 // 네임스페이스 선언
             Operator.Run(); // Operator 클래스의 run 메서드 호출
             Arrays.Run();    // Array 클래스의 run 메서드 호출
             ConditionalStatement.Run(); // ConditionalStatement 클래스의 run 메서드 호출
+            LoopStatement.Run(); // LoopStatement 클래스의 run 메서드 호출
 
         }
     }
